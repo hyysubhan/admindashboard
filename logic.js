c8fdf2287c391c11ab70ac35fff62d16dd49437f
hyysubhan <subhanarha16@gmail.com>
2025-08-01 09:46:58 +0500

    Updated files and made changes

--- logic.js
+++ logic.js
@@ -1,23 +1,8 @@
-let noti = document.querySelector('.noitfy')
-
-noti.addEventListener('click', function() {
-    alert("No New Notification");
+let noti = document.querySelector('.notify')
+noti.addEventListener('click', function(){
+    alert("NO NEW NOTIFICATION");
 });
 let msg = document.querySelector('.mail')
-
-msg.addEventListener('click', function() {
-    alert("No New Message");
+msg.addEventListener('click', function(){
+    alert("NO NEW MESSAGE");
 });
-// this funtion is for notify 
-  let button = document.querySelector('.search-btn');
-  let searchInputBox = document.querySelector('.search');
-
-  button.addEventListener('click', function(){
-    // Toggle the visibility
-    if (searchInputBox.style.display === 'none') {
-      searchInputBox.style.display = 'block';
-    } else {
-      searchInputBox.style.display = 'none';
-    }
-  });
-
